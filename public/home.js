544c630f9da4e48ca1e7643569a8192cfc452a56
Bill Ma <billma127@gmail.com>
2012-09-20 01:31:00 -0700

    final

--- public/home.js
+++ public/home.js
@@ -10,48 +10,59 @@ $(function(){
   
   listTopics=function(){
     topics.on('value',function(data){
-      $('#topList .container').html('')
+       console.log(data.val())
+      $('#listContainer').html('')
       data.forEach(addTopicLink)
+     
     })
   }
   
   
   addTopic=function(name,session_id){
     // check to see if topic already exist
+    name=name.toLowerCase()
     topics.once('value',function(data){
-      name=name.toLowerCase()
-      var test=name
+      
+      // var test=name
       if(!data.hasChild(name)){
-        
         var newTopicRef=new Firebase('http://gamma.firebase.com/song/firebaseRoulette/topics/'+name)
-        newTopicRef.child('name').set(name)
-        newTopicRef.child('session_id').set(session_id)
-        newTopicRef.child('totalUser').set(0)
-        
-      }
-      else
+        newTopicRef.set({name:name,session_id:session_id,totalUser:1}, function(){
+           window.location="/"+name;
+        })
+      }else{
         alert('Topic: '+name+'already exist')
-      // window.location="/"+test;
+      } 
+  
     })
-    
   }
   addTopicLink=function(topic){
     topicName=topic.val()['name'].toLowerCase()
     totalUser=topic.val()['totalUser']
-    var template='<a href="/'+topicName+'" topic="'+topicName+'"class="each_room btn">'+
-            '<i class="icon-group"></i>'+
-            '<span class="each_room_name">'+topicName+'</span>'+
-            '<span class="totalUser">'+totalUser+'</span>'+
-          '</a>' 
-    $('#topList .container').append(template)
+    // if(totalUser=="0"){
+      // var emptyTopicRef=new Firebase('http://gamma.firebase.com/song/firebaseRoulette/topics/'+name)
+      // emptyTopicRef.remove()
+    // }else{
+      var template='<a href="/'+topicName+'" topic="'+topicName+'"class="each_room btn">'+
+        '<i class="icon-group"></i>'+
+        '<span class="each_room_name">'+topicName+'</span>'+
+        '<span class="totalUser">'+totalUser+'</span>'+
+      '</a>' 
+      // $('#topList .container').append(template) 
+      
+      $('#listContainer').append(template)
+      
+    // }
+
+  
+
   }
   function submitTopic(){
     var name=$('#topic_input').val()
     $.post( '/getSession',{},function(data){
       if(data['session_id']!=null){
-          console.log(data['session_id'])
           addTopic(name, data['session_id'])
-          
+          // alert(data['session_id'])
+          // window.location="/"+name;
       }
     })
   }
